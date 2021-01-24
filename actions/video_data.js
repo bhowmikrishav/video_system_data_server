5bd5ea193b61b905b046fa32dff81605c17cb6a3
Rishav Bhowmik <tgs.rishav.bhowmik@gmail.com>
2021-01-24 15:08:04 +0530

    # VideoData debug with duration projection

--- actions/video_data.js
+++ actions/video_data.js
@@ -20,7 +20,12 @@ class VideoData{
             {limit}
         ).project({title:1, upload_time:1, "stream_manifest.144.duration":1, "stream_manifest.360.duration":1, "stream_manifest.720.duration":1}).toArray()
         return results.map(r=>{
-            r.duration = r.stream_manifest["144"].duration||r.stream_manifest["360"].duration||r.stream_manifest["720"].duration
+            try{
+                r.duration = r.stream_manifest["144"].duration||r.stream_manifest["360"].duration||r.stream_manifest["720"].duration
+            }catch(e){
+                r.duration = null
+            }
+            
             r._id = r._id.toString()
             return r
         })
